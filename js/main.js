d99dea57eb6bfdf6bc4d37f35bae5d6010ca3faf
dev-wf <w.a.f@outlook.com.br>
2022-07-22 21:51:49 -0300

    up

---
+++ js/main.js
@@ -0,0 +1,18 @@
+console.log("mains.js");
+
+$("#btnum").click(function () {
+    alert('CLICOU BUTTON 1');
+});
+
+$("#btndois").click(function () {
+    alert('CLICOU BUTTON 2');
+});
+$("#btntres").click(function () {
+    alert('CLICOU BUTTON 3');
+});
+
+$("#btnquatro").click(function () {
+    alert('CLICOU BUTTON 4');
+    window.location.assign("../pages/default.html");
+
+});
