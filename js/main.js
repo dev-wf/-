4a36012dcf8252ec41bfe92d8f4e2f0349914d0c
dev-wf <w.a.f@outlook.com.br>
2022-07-22 22:26:32 -0300

    up

--- js/main.js
+++ js/main.js
@@ -1,18 +1,2 @@
 console.log("mains.js");
 
-$("#btnum").click(function () {
-    alert('CLICOU BUTTON 1');
-});
-
-$("#btndois").click(function () {
-    alert('CLICOU BUTTON 2');
-});
-$("#btntres").click(function () {
-    alert('CLICOU BUTTON 3');
-});
-
-$("#btnquatro").click(function () {
-    alert('CLICOU BUTTON 4');
-    location.href = "/recibo.html";
-
-});
