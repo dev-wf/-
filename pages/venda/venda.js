3a9e532ac0327e86b71f9e303ebd579a1736df2c
dev-wf <w.a.f@outlook.com.br>
2022-07-25 22:52:11 -0300

    up

--- pages/venda/venda.js
+++ pages/venda/venda.js
@@ -1,2 +1,10 @@
 console.log("venda.js");
+//CRUD 
+var frutas = ['Maçã', 'Banana'];
+import db from './venda.json' assert {type: 'json'}
+// C = CREATE, R = READ, U = UPDATE, D = DELETE
+var prod = db.produtos;
+var usu = db.usuarios;
+
+console.log(prod);
 
