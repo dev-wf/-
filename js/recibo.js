6e3f8c48df32fb59d07630e51a945f11705f06d4
dev-wf <w.a.f@outlook.com.br>
2022-07-29 00:54:23 -0300

    up

--- js/recibo.js
+++ js/recibo.js
@@ -1,6 +1,23 @@
 console.log("recibo.js");
 const date = new Date();
-var datahj = date.toLocaleDateString();
+var SetdataNow = date.toLocaleDateString();
+var setData = document.getElementById("setData");
+var getData = document.getElementById("getData");
+var setValor = document.getElementById("setValor");
+var getValor = document.getElementById("getValor");
+var setCliente = document.getElementById("setCliente");
+var getCliente = document.getElementById("getCliente");
+var setClienteDoc = document.getElementById("setClienteDoc");
+var getClienteDoc = document.getElementById("getClienteDoc");
+var setReferente = document.getElementById("setReferente");
+var getReferente = document.getElementById("getReferente");
+var setEmissor = document.getElementById("setEmissor");
+var getEmissor = document.getElementById("getEmissor");
+var setEmissorDoc = document.getElementById("setEmissorDoc");
+var getEmissorDoc = document.getElementById("getEmissorDoc");
+// 
+
+
 
 
 
@@ -16,8 +33,20 @@ $("#print").submit(function (event) {
 */
 
 $("#print").submit(function (e) {
-    console.log("CLICOU IMPRIMIR");
-    e.preventDefault();
+    if (setData.value == "") {
+        getData.innerText = SetdataNow;
+    } else {
+        getData.innerText = setData.value;
+    }
+    getValor.innerText = setValor.value;
+    getCliente.innerText = setCliente.value;
+    getClienteDoc.innerText = setClienteDoc.value;
+    getReferente.innerText = setReferente.value;
+    getEmissor.innerText = setEmissor.value;
+    getClienteDoc.innerText = setEmissorDoc.value;
 
+
+
+    e.preventDefault();
 });
 
